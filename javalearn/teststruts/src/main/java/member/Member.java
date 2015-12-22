8b1065b90ad05ba27c1b2085a2936be1c0222c3a
xing020010 <xing020010@hotmail.com>
2015-12-22 17:48:15 +0800

    开始添加hibernate

---
+++ javalearn/teststruts/src/main/java/member/Member.java
@@ -0,0 +1,39 @@
+package member;
+
+public class Member {
+	private int id;
+	private String username;
+	private String password;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Member() {
+	}
+
+	public Member(String username, String password) {
+		this.username = username;
+		this.password = password;
+	}
+}
